9f417f9a9380fb94cb33ebc9754e59734eb7b00c
Mounika <mounikaboja2403@gmail.com>
2026-05-28 16:36:07 +0530

    Fixed spacy compatibility

--- backend/interview_generator.py
+++ backend/interview_generator.py
@@ -1,9 +1,20 @@
 import pandas as pd
+import os
+
+# Base directory
+BASE_DIR = os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))
+)
+
+# Dataset path
+dataset_path = os.path.join(
+    BASE_DIR,
+    "datasets",
+    "interview_questions.csv"
+)
 
 # Load interview dataset
-questions_df = pd.read_csv(
-    "../datasets/interview_questions.csv"
-)
+questions_df = pd.read_csv(dataset_path)
 
 def generate_questions(resume_skills):
 
