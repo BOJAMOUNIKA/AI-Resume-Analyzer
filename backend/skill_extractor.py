bd97cd30bcb87591bd1d95d58e8ce5feed228d63
Mounika <mounikaboja2403@gmail.com>
2026-05-28 16:22:27 +0530

    Fixed spacy compatibility

--- backend/skill_extractor.py
+++ backend/skill_extractor.py
@@ -1,11 +1,7 @@
 import pandas as pd
-import spacy
-
-# Load NLP model
-nlp = spacy.load("en_core_web_sm")
 
 # Load skills dataset
-skills_df = pd.read_csv("../datasets/skills.csv")
+skills_df = pd.read_csv("datasets/skills.csv")
 
 # Convert skills into list
 skills_list = skills_df['skill'].tolist()
@@ -14,7 +10,6 @@ def extract_skills(text):
 
     detected_skills = []
 
-    # Convert text to lowercase
     text = text.lower()
 
     for skill in skills_list:
